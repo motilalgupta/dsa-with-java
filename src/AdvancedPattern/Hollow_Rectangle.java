1ecea5bfc0ae4de55ef6f5dfcaed8c88a2e273ab
Motilal Gupta <motilalgupta943071@gmail.com>
2026-05-10 22:09:48 +0530

    Implimented Hollow_Rectangle pattern using method

---
+++ src/AdvancedPattern/Hollow_Rectangle.java
@@ -0,0 +1,19 @@
+package AdvancedPattern;
+
+public class Hollow_Rectangle {
+    public static void main(String[] args) {
+        hollowRectangle(4,5);
+    }
+    public static void hollowRectangle(int row, int column){
+        for(int i = 1; i<=row; i++){
+            for(int j = 1; j<=column; j++){
+                if(i == 1 || i == row || j == 1 || j == column){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
