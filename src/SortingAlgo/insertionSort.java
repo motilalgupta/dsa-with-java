4fa69f4bdcd8a40910cd1a789e2becbb712a7fab
Motilal Gupta <motilalgupta943071@gmail.com>
2026-06-29 22:57:26 +0530

    Add Insertion Sort implementation

---
+++ src/SortingAlgo/insertionSort.java
@@ -0,0 +1,22 @@
+package SortingAlgo;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        selectionSort(arr);
+    }
+    public static void selectionSort(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
