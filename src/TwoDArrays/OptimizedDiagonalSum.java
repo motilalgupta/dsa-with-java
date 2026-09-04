a40f11dab74cd89044c1e3a81f7de9ee1d5d757c
Motilal Gupta <motilalgupta943071@gmail.com>
2026-09-05 01:15:04 +0530

    Optimize diagonal sum calculation using a single loop

--- src/TwoDArrays/OptimizedDiagonalSum.java
+++ src/TwoDArrays/OptimizedDiagonalSum.java
@@ -1,6 +1,6 @@
 package TwoDArrays;
 
-public class DiagonalSum {
+public class OptimizedDiagonalSum {
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         diagonalSum(matrix);
@@ -9,14 +9,11 @@ public class DiagonalSum {
         int sum = 0;
         // primary diagonal
         for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[0].length; j++){
-                if(i == j){
-                    sum += matrix[i][j];
-                }else if(i + j == matrix.length-1){
-                    sum += matrix[i][j];
-                }
+            sum += matrix[i][i];
+            if(i != matrix.length-1-i){
+                sum += matrix[i][matrix.length-1-i];
             }
         }
-        System.out.println("Total sum is: "+sum);
+        System.out.println("The sum is: "+sum);
     }
 }
